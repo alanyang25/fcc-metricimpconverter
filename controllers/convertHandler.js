96af79c832809a92ada39a10faa2d9893d1c5a1a
alanyang25 <61829671+calthorp@users.noreply.github.com>
2021-05-18 15:07:10 +0800

    updates

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,13 +1,46 @@
 function ConvertHandler() {
+
+  function numberStringSplitter(input) {
+    let number = input.match(/[.\d\/]+/g) || ["1"];
+    let string = input.match(/[a-zA-Z]+/g)[0];
+
+    return [number[0], string];
+  }
+
+  function checkDiv(possibleFraction) {
+    // 9/4/2 return false
+
+    // 25 return ["25"]
+
+    // 3/5 return ["3", "5"]
+    let nums = possibleFraction.split("/");
+    if (nums.length > 2) {
+      return false;
+    }
+    return nums;
+  }
   
   this.getNum = function(input) {
-    let result;
-    
+    let result = numberStringSplitter(input)[0];
+    let nums = checkDiv(result);
+    if (!nums) {
+      return undefined;
+    }
+
+    num1 = nums[0];
+    num2 = nums[1] || "1";
+
+    if (isNaN(num1) || isNaN(num2)) { // 會先將 x 轉換成數字類型 → .isNaN(Number(x))，再進行比對是否為 NaN(Not a number)
+      return undefined;
+    }
+
+    result = (parseFloat(num1) / parseFloat(num2)).toFixed(5);
+
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result = numberStringSplitter(input)[1];
     
     return result;
   };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -4,7 +4,11 @@ let assert = chai.assert;
 const server = require('../server');
 
 chai.use(chaiHttp);
-
+// Convert a valid input such as 10L: GET request to /api/convert.
+// Convert an invalid input such as 32g: GET request to /api/convert.
+// Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.
+// Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.
+// Convert with no number such as kg: GET request to /api/convert.
 suite('Functional Tests', function() {
 
 });
